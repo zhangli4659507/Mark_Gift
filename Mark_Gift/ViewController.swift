9261a61453a2ebdedbf641e1fa5cbccb5163bba1
Mark <365764028@qq.com>
2016-07-22 15:37:50 +0800

    Initial Commit

---
+++ Mark_Gift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mark_Gift
+//
+//  Created by Mark on 16/7/22.
+//  Copyright © 2016年 Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
